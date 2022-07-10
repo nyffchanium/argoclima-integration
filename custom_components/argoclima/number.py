23a93fcca68f63f0c1cdfb2894508245bc7f0e51
Stefan Nyffenegger <stefan.nyffenegger@nyffchanium.com>
2022-07-10 17:40:32 +0200

    update deprecated usage of NumberEntity (fix #6)

--- custom_components/argoclima/number.py
+++ custom_components/argoclima/number.py
@@ -7,6 +7,7 @@ from custom_components.argoclima.entity import ArgoEntity
 from homeassistant.components.number import NumberEntity
 from homeassistant.components.number import NumberMode
 from homeassistant.config_entries import ConfigEntry
+from homeassistant.const import PERCENTAGE
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers.entity import EntityCategory
 
@@ -38,34 +39,38 @@ class ArgoEcoLimitNumber(ArgoEntity, NumberEntity):
         return "mdi:leaf"
 
     @property
-    def value(self) -> float:
+    def native_value(self) -> float:
         if not self._type.eco_limit:
             raise InvalidOperationError
         return self.coordinator.data.eco_limit
 
     @property
-    def min_value(self) -> int:
+    def native_min_value(self) -> int:
         if not self._type.eco_limit:
             raise InvalidOperationError
         return self._type.eco_limit_min
 
     @property
-    def max_value(self) -> int:
+    def native_max_value(self) -> int:
         if not self._type.eco_limit:
             raise InvalidOperationError
         return self._type.eco_limit_max
 
     @property
-    def step(self) -> int:
+    def native_step(self) -> int:
         if not self._type.eco_limit:
             raise InvalidOperationError
         return 1
 
     @property
+    def native_unit_of_measurement(self) -> str:
+        return PERCENTAGE
+
+    @property
     def mode(self) -> NumberMode:
         return NumberMode.BOX
 
-    async def async_set_value(self, value: float) -> None:
+    async def async_set_native_value(self, value: float) -> None:
         if not self._type.eco_limit:
             raise InvalidOperationError
         self.coordinator.data.eco_limit = int(value)
